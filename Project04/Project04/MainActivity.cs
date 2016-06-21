5614725063bb871046a158e626ec468829c6a349
W1lco <wilco1999muit@gmail.com>
2016-06-21 14:49:35 +0200

    Start

---
+++ Project04/Project04/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Project04
+{
+    [Activity(Label = "Project04", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            Button button2 = FindViewById<Button>(Resource.Id.MyButton2);
+            
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            button.Click += delegate { button2.Visibility = Android.Views.ViewStates.Invisible; };
+        }
+    } 
+}
+
