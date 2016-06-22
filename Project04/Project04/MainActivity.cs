2ba52427e102208622f86ed45a65753b9a981cb0
W1lco <wilco1999muit@gmail.com>
2016-06-22 12:01:39 +0200

    Commit

--- Project04/Project04/MainActivity.cs
+++ Project04/Project04/MainActivity.cs
@@ -5,28 +5,76 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Project04
 {
     [Activity(Label = "Project04", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
-
-        protected override void OnCreate(Bundle bundle)
+       protected override void OnCreate(Bundle bundle)
         {
+            //fietstrommel fietstrommel = new fietstrommel();
             base.OnCreate(bundle);
-
+            
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
             // Get our button from the layout resource,
             // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.MyButton);
-            
-            
-            button.Click += delegate { button.Visibility = Android.Views.ViewStates.Invisible; };
+
+
+            button.Click += delegate
+            {
+                button.Text = "hallo";
+            };
+
+
+
+
+
+            //button.Visibility = Android.Views.ViewStates.Invisible;
         }
-    } 
+    }
+    class fietstrommel
+    {
+        public static List<string> fietsnummer = new List<string>();
+        public static List<string> FT = new List<string>();
+        public static List<string> Fietstrommel = new List<string>();
+        public static List<string> straat = new List<string>();
+        public static List<string> thv = new List<string>();
+        public static List<string> Xcoord = new List<string>();
+        public static List<string> Ycoord = new List<string>();
+        public static List<string> deelgem = new List<string>();
+        public static List<string> status = new List<string>();
+        public static List<string> datum = new List<string>();
+
+        public fietstrommel()
+        {
+
+
+            var reader = new StreamReader(File.OpenRead(@"Data\fietstrommel.csv"));
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+                //var values2 = values.Split(',');
+
+                fietstrommel.fietsnummer.Add(values[0]);
+                fietstrommel.FT.Add(values[1]);
+                fietstrommel.Fietstrommel.Add(values[2]);
+                fietstrommel.straat.Add(values[3]);
+                fietstrommel.thv.Add(values[4]);
+                fietstrommel.Xcoord.Add(values[5]);
+                fietstrommel.Ycoord.Add(values[6]);
+                fietstrommel.deelgem.Add(values[7]);
+                fietstrommel.status.Add(values[8]);
+                fietstrommel.datum.Add(values[9]);
+            }
+        }
+    }
 }
 
--- Project04/Project04/Resources/Resource.Designer.cs
+++ Project04/Project04/Resources/Resource.Designer.cs
@@ -63,6 +63,9 @@ namespace Project04
 			// aapt resource value: 0x7f050000
 			public const int MyButton = 2131034112;
 			
+			// aapt resource value: 0x7f050001
+			public const int imageView1 = 2131034113;
+			
 			static Id()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
