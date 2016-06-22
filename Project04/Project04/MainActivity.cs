8be85eb457c0b43e1371ae2d2b04db675534a6a2
W1lco <wilco1999muit@gmail.com>
2016-06-22 12:21:00 +0200

    ...

--- Project04/Project04/MainActivity.cs
+++ Project04/Project04/MainActivity.cs
@@ -15,7 +15,7 @@ namespace Project04
     {
        protected override void OnCreate(Bundle bundle)
         {
-            //fietstrommel fietstrommel = new fietstrommel();
+            fietstrommel fietstrommel = new fietstrommel();
             base.OnCreate(bundle);
             
             // Set our view from the "main" layout resource
@@ -25,12 +25,57 @@ namespace Project04
             // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.MyButton);
 
+
             button.Click += delegate
             {
-                button.Text = "hallo";
+                
+                button.Text = fietstrommel.straat[25];
             };
+
+
+
+
+
             //button.Visibility = Android.Views.ViewStates.Invisible;
         }
-    }  
+    }
+    class fietstrommel
+    {
+        public static List<string> fietsnummer = new List<string>();
+        public static List<string> FT = new List<string>();
+        public static List<string> Fietstrommel = new List<string>();
+        public static List<string> straat = new List<string>();
+        public static List<string> thv = new List<string>();
+        public static List<string> Xcoord = new List<string>();
+        public static List<string> Ycoord = new List<string>();
+        public static List<string> deelgem = new List<string>();
+        public static List<string> status = new List<string>();
+        public static List<string> datum = new List<string>();
+
+        public fietstrommel()
+        {
+
+
+            var reader = new StreamReader(File.OpenRead(@"drawable\fietstrommel.csv"));
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+                //var values2 = values.Split(',');
+
+                fietstrommel.fietsnummer.Add(values[0]);
+                fietstrommel.FT.Add(values[1]);
+                fietstrommel.Fietstrommel.Add(values[2]);
+                fietstrommel.straat.Add(values[3]);
+                fietstrommel.thv.Add(values[4]);
+                fietstrommel.Xcoord.Add(values[5]);
+                fietstrommel.Ycoord.Add(values[6]);
+                fietstrommel.deelgem.Add(values[7]);
+                fietstrommel.status.Add(values[8]);
+                fietstrommel.datum.Add(values[9]);
+            }
+        }
+    }
 }
 
