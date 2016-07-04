91fe97525970f040f12b805b0996e5ddc59d826d
TareqSaleem <tareqy2003@hotmail.com>
2016-07-04 16:29:25 +0200

    Update 2.0

--- Slide Wilco/app/src/main/java/com/t/mr/slide/MainActivity.java
+++ Slide Wilco/app/src/main/java/com/t/mr/slide/MainActivity.java
@@ -37,8 +37,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Calendar;
-
-
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
     public static Button button;
     public static Button button2;
@@ -58,10 +56,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public LocationManager mLocationManager;
     public static WebView webView;
     public static ImageButton imageButton;
-
     public static ArrayList<Integer> intList = new ArrayList<Integer>();
     public static ArrayList<Integer> intListRec = new ArrayList<Integer>();
-
     public void enableStrictMode()
     {        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 
@@ -69,18 +65,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         enableStrictMode();
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         String url ="http://rotterdamopendata.nl/dataset";
         WebView view=(WebView) this.findViewById(R.id.webView);
         view.getSettings().setJavaScriptEnabled(true);
         view.loadUrl(url);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(this)
                     .addConnectionCallbacks(this)
@@ -88,7 +80,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                     .addApi(LocationServices.API)
                     .build();
         }
-
         this.webView = (WebView) findViewById(R.id.webView);
         this.button = (Button) findViewById(R.id.button);
         this.button2 = (Button) findViewById(R.id.button2);
@@ -104,39 +95,30 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         this.buttonLocation1 = (Button) findViewById(R.id.button6);
         this.buttonLocation2 = (Button) findViewById(R.id.button7);
         this.imageButton = (ImageButton) findViewById(R.id.imageButton);
-
         np2.setMaxValue(2013); np2.setMinValue(2010); np2.setValue(2010);
         String SQL = "Select DISTINCT fietsdiefstal.Buurt, fietstrommel.Deelgem as buur FROM fietstrommel, fietsdiefstal WHERE fietstrommel.Deelgem = fietsdiefstal.Buurt";
         Query_2 querry = new Query_2();
         ResultSet res = querry.getQueryResult(SQL);
         ArrayList<String> buurt = new ArrayList<>();
         buurt.add("CENTRUM"); buurt.add("DELFSHAVEN"); buurt.add("FEIJENOORD"); buurt.add("OVERSCHIE"); buurt.add("PERNIS");
-
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item, buurt);
         spinner.setAdapter(adapter);
-
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
         int LOCATION_REFRESH_TIME = 1000;
         int LOCATION_REFRESH_DISTANCE = 5;
-
         if ( Build.VERSION.SDK_INT >= 23 &&
                 ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
                 ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return  ;
         }
-
         mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME, LOCATION_REFRESH_DISTANCE, mLocationListener);
-
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -168,7 +150,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                     }
                 }catch(Exception e){
                     e.printStackTrace();
-
                 }
                 Intent intent = new Intent(MainActivity.this, pie_chart.class);
                 startActivity(intent);
@@ -242,8 +223,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                         mGoogleApiClient);
                 if (mLastLocation != null) {
                 }
-
-
                 Locate.getAddressFromLocation(mLastLocation.getLatitude(),mLastLocation.getLongitude(),MainActivity.this);
                 try{
                     Thread.sleep(500);
@@ -260,37 +239,29 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         });
     }
-
     public void onAddEventClicked(View view){
         Intent intent = new Intent(Intent.ACTION_INSERT);
         intent.setType("vnd.android.cursor.item/event");
-
         Calendar cal = Calendar.getInstance();
         long startTime = cal.getTimeInMillis();
         long endTime = cal.getTimeInMillis()  + 60 * 60 * 1000;
-
         intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, startTime);
         intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,endTime);
         intent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
-
         intent.putExtra(CalendarContract.Events.TITLE, "Gestolen fiets");
         intent.putExtra(CalendarContract.Events.DESCRIPTION,  "Description");
         intent.putExtra(CalendarContract.Events.EVENT_LOCATION, "My location");
         intent.putExtra(CalendarContract.Events.RRULE, "FREQ=YEARLY");
-
         startActivity(intent);
     }
-
     protected void onStart() {
         mGoogleApiClient.connect();
         super.onStart();
     }
-
     protected void onStop() {
         mGoogleApiClient.disconnect();
         super.onStop();
     }
-
     @Override
     public void onConnected(Bundle bundle) {
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
@@ -298,17 +269,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if (mLastLocation != null) {
         }
     }
-
     @Override
     public void onConnectionSuspended(int i) {
 
     }
-
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
 
     }
-
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -318,21 +286,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             super.onBackPressed();
         }
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
@@ -343,9 +308,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(MenuItem menuItem) {
         TextView textView1 = (TextView) findViewById(R.id.textView);
-
+        DrawerLayout nav_view = (DrawerLayout) findViewById(R.id.drawer_layout);
         switch (menuItem.getItemId()) {
-
             // Handle navigation view item clicks here.
             case R.id.nav_pie_chart:
                 button.setVisibility(View.VISIBLE);
@@ -361,8 +325,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 spinner.setVisibility(View.INVISIBLE);
                 buttonLocation1.setVisibility(View.INVISIBLE);
                 buttonLocation2.setVisibility(View.INVISIBLE);
+                webView.setVisibility(View.INVISIBLE);
+                nav_view.closeDrawers();
                 return true;
-
             case R.id.nav_line_chart:
                 menuItem.setChecked(true);
                 textViewLine2.setVisibility(View.VISIBLE);
@@ -380,15 +345,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 buttonLocation1.setVisibility(View.INVISIBLE);
                 buttonLocation2.setVisibility(View.INVISIBLE);
                 webView.setVisibility(View.INVISIBLE);
+                nav_view.closeDrawers();
                 return true;
-
             case R.id.nav_bar_chart:
                 textBar1.setVisibility(View.VISIBLE);
                 textBar2.setVisibility(View.VISIBLE);
                 buttonBar1.setVisibility(View.VISIBLE);
                 buttonBar2.setVisibility(View.VISIBLE);
                 spinner.setVisibility(View.VISIBLE);
-
                 button.setVisibility(View.INVISIBLE);
                 button2.setVisibility(View.INVISIBLE);
                 textView.setVisibility(View.INVISIBLE);
@@ -398,13 +362,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 buttonLocation1.setVisibility(View.INVISIBLE);
                 buttonLocation2.setVisibility(View.INVISIBLE);
                 webView.setVisibility(View.INVISIBLE);
+                nav_view.closeDrawers();
                 return true;
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
     private final android.location.LocationListener mLocationListener = new android.location.LocationListener() {
         @Override
         public void onLocationChanged(Location location) {
@@ -427,5 +391,4 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             //turns off gps services
         }
     };
-
 }
